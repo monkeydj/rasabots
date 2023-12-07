993e2b56d57dd06b61b8145a430b1ef5c59ec309
jiotaro <monkeydjiotaro@gmail.com>
2023-12-08 00:20:29 +0700

    day.2 add LOADED_CUBES and first input parsing

--- day_2.py
+++ day_2.py
@@ -3,8 +3,12 @@ This adheres a solution of Day 2 puzzle - https://adventofcode.com/2023/day/2
 """
 
 from os import getcwd
+import re
 
 input_file = f"{getcwd()}/input.txt"
+
+LOADED_CUBES = {"red": 12, "green": 13, "blue": 14}
+
 answer = None
 
 with open(input_file) as fd:
@@ -13,6 +17,11 @@ with open(input_file) as fd:
         if not input_line:
             break
 
-        print(input_line.strip())
+        print(input_line.strip(), end=" --> ")
+
+        game_id, records = input_line.strip().split(": ")
+        game_id = int(game_id.replace("Game", "").strip())
+        records = re.split("; ", records)
+        print(f"[#{game_id}] {records}")
 
 print(f"[[[ Final Answer Is: {answer} ]]]")
