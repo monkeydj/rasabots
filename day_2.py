e410f37eb2157a9aab8917914c083cdf234a81a5
jiotaro <monkeydjiotaro@gmail.com>
2023-12-10 22:59:46 +0700

    day.2 extensively use generator with a nested loop

--- day_2.py
+++ day_2.py
@@ -5,23 +5,42 @@ This adheres a solution of Day 2 puzzle - https://adventofcode.com/2023/day/2
 from os import getcwd
 import re
 
-LOADED_CUBES = {"red": 12, "green": 13, "blue": 14}
+CUBES_IN_BAG = {"red": 12, "green": 13, "blue": 14}
 
 answer = 0  # hold the expected output
 
 input_file = f"{getcwd()}/input.txt"
 inputs = (i for i in open(input_file))
 
+
+def extract(game_inputs):
+    for data in re.split("; ", game_inputs):
+        elf_s_hand = re.findall(r'(\d+) (blue|red|green)', data)
+        print(f" --> {elf_s_hand}", end=" --> ...")
+
+        for cube in elf_s_hand:
+            yield cube
+
+
+def is_possible(game_inputs):
+    for count, cube_color in extract(game_inputs):
+        if int(count) > CUBES_IN_BAG.get(cube_color, 0):
+            print("ERRORED!!")
+            return False
+        else:
+            print("PASS")
+
+    return True
+
+
 for input_line in inputs:
     input_line = input_line.strip()
     print(input_line)
 
-    game_id, records = input_line.split(": ")
+    game_id, game_inputs = input_line.split(": ")
     game_id = int(game_id.replace("Game", "").strip())
 
-    for rec in re.split("; ", records):
-        print(f" --> [#{game_id}] {rec}", end=" --> ...\n")
-
-    answer += game_id
+    if is_possible(game_inputs):
+        answer += game_id
 
 print(f"[[[ Final Answer Is: {answer} ]]]")
