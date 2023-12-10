eafe9017d4ff3b782788ec2a46a2be229a201cbe
jiotaro <monkeydjiotaro@gmail.com>
2023-12-10 22:33:06 +0700

    day.2 refactor reading inputs and mock the rough answer

--- day_2.py
+++ day_2.py
@@ -5,23 +5,21 @@ This adheres a solution of Day 2 puzzle - https://adventofcode.com/2023/day/2
 from os import getcwd
 import re
 
-input_file = f"{getcwd()}/input.txt"
-
 LOADED_CUBES = {"red": 12, "green": 13, "blue": 14}
 
-answer = None
+answer = 0  # hold the expected output
 
-with open(input_file) as fd:
-    while True:
-        input_line = fd.readline()
-        if not input_line:
-            break
+input_file = f"{getcwd()}/input.txt"
+inputs = (i for i in open(input_file))
 
-        print(input_line.strip(), end=" --> ")
+for input_line in inputs:
+    print(input_line.strip(), end=" --> ")
 
-        game_id, records = input_line.strip().split(": ")
-        game_id = int(game_id.replace("Game", "").strip())
-        records = re.split("; ", records)
-        print(f"[#{game_id}] {records}")
+    game_id, records = input_line.strip().split(": ")
+    game_id = int(game_id.replace("Game", "").strip())
+    records = re.split("; ", records)
+    print(f"[#{game_id}] {records}")
+
+    answer += game_id
 
 print(f"[[[ Final Answer Is: {answer} ]]]")
