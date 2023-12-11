b0b4d952f53cce2b1f06e2dbd0e02c71eacc06a0
jiotaro <monkeydjiotaro@gmail.com>
2023-12-11 09:09:17 +0700

    day.2#2 refactor logic to solve part two

--- day_2_two.py
+++ day_2_two.py
@@ -5,27 +5,24 @@ This adheres a solution of Day 2 puzzle - https://adventofcode.com/2023/day/2
 from os import getcwd
 import re
 
-CUBES_IN_BAG = {"red": 12, "green": 13, "blue": 14}
+
+def get_cube_powers(game_inputs):
+    colored_cubes = re.findall(r'(\d+) (blue|red|green)', game_inputs)
+    cubes_posibility = {"red": 0, "green": 0, "blue": 0}
+
+    for count, cube_color in colored_cubes:
+        print(f" --> {cube_color}: {count}", end=" --> ...")
+        # TODO: update max value in cubes_posibility
+
+    return cubes_posibility.get("red") * \
+        cubes_posibility.get("green") * \
+        cubes_posibility.get("blue")
+
 
 answer = 0  # hold the expected output
 
 input_file = f"{getcwd()}/input.txt"
 inputs = (i for i in open(input_file))
-
-
-def get_powers(game_inputs):
-    colored_cubes = re.findall(r'(\d+) (blue|red|green)', game_inputs)
-
-    for count, cube_color in colored_cubes:
-        print(f" --> {cube_color}: {count}", end=" --> ...")
-
-        if int(count) > CUBES_IN_BAG.get(cube_color, 0):
-            print("ERRORED!!")
-            return 0
-
-        print("PASS")
-
-    return 1
 
 
 for input_line in inputs:
@@ -35,6 +32,6 @@ for input_line in inputs:
     game_id, game_inputs = input_line.split(": ")
     game_id = int(game_id.replace("Game", "").strip())
 
-    answer += get_powers(game_inputs)
+    answer += get_cube_powers(game_inputs)
 
 print(f"[[[ Final Answer Is: {answer} ]]]")
