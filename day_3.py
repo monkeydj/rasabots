744af97b03d14274b8317d89224715fd45a0f5ee
jiotaro <monkeydjiotaro@gmail.com>
2023-12-12 13:59:00 +0700

    day.3 reorder LoCs and approach with prev_input

--- day_3.py
+++ day_3.py
@@ -6,18 +6,21 @@ This adheres a solution of Day 3 puzzle
 from os import getcwd
 import re
 
+input_file = f"{getcwd()}/input.txt"
+inputs = (i for i in open(input_file))
+
 symbols = r'[^\w\.]'  # not alphanumeric and dot
 
 answer = 0  # hold the expected output
-
-input_file = f"{getcwd()}/input.txt"
-inputs = (i for i in open(input_file))
+prev_input = None  # track previously loaded input
 
 for input_line in inputs:
     input_line = input_line.strip()
     print(input_line, end=" --> ")
 
     print(f"sym-ctrl: {re.findall(symbols, input_line)}")
+
+    prev_input = input_line
 
     # answer += game_id
 
