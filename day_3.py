9039af2fbd6b96dc8056ef6335c7cbc2b1c2b99a
jiotaro <monkeydjiotaro@gmail.com>
2023-12-12 21:55:00 +0700

    day.3 implement prev_symbols but still not quite right

--- day_3.py
+++ day_3.py
@@ -15,6 +15,7 @@ answer = 0  # hold the expected output
 
 # TODO: make a closure to not have this var 'globally'
 prev_input = None
+prev_symbols = []
 
 
 def find_part_numbers(input_line: str) -> int:
@@ -22,12 +23,11 @@ def find_part_numbers(input_line: str) -> int:
 
     for sym in re.finditer(symbols, input_line):
         span = sym.span()
+        prev_symbols.append(span)  # track found symbols
 
         if prev_input is not None:
             prev_slice = prev_input[span[0] - 3:span[1] + 3]
             part_numbers += re.findall(r'\d+', prev_slice)
-        # above if is like a lookbehind for any desired numbers
-        # TODO: implement lookahead with prev_symbols
 
         # find numbers before or after symbols (not dot)
         input_slice = input_line[span[0] - 3:span[1] + 3]
@@ -40,10 +40,21 @@ for input_line in inputs:
     input_line = input_line.strip()
     print(input_line, end=" ---> ")
 
-    parts_sum = find_part_numbers(input_line)
+    parts_sum = 0
+
+    while len(prev_symbols) > 0:
+        prev_span, prev_symbols = prev_symbols[0], prev_symbols[1:]
+        input_slice = input_line[prev_span[0] - 3:prev_span[1] + 3]
+        new_part_numbers = re.findall(r'\d+', input_slice)
+        parts_sum += sum([int(x) for x in new_part_numbers])
+        print(parts_sum, end=" + ")
+
+    parts_sum += find_part_numbers(input_line)
     print(parts_sum)
 
     answer += parts_sum
+
+    prev_input = input_line  # track previously loaded
 
 
 print(f"[[[ Final Answer Is: {answer} ]]]")
