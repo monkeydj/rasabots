e2833209d5143c589fbd281487ab89b21dee19e7
jiotaro <monkeydjiotaro@gmail.com>
2023-12-12 22:06:03 +0700

    day.3 refactor find_part_numbers but it starts to feel tiring..

--- day_3.py
+++ day_3.py
@@ -18,22 +18,32 @@ prev_input = None
 prev_symbols = []
 
 
-def find_part_numbers(input_line: str) -> int:
-    symbols, part_numbers = r'[^\w\.]', []
+def find_part_numbers(span: [int, int], engine_line: str) -> int:
+    """
+    Find numbers around a symbol span.
+    """
+    engine_slice = engine_line[span[0] - 3:span[1] + 3]
+    engine_numbers = re.findall(r'\d+', engine_slice)
 
-    for sym in re.finditer(symbols, input_line):
+    return sum([int(x) for x in engine_numbers])
+
+
+def unwind_schemetic(engine_line: str) -> int:
+    """
+    Try to unwind each line & prev. tracked line of the engine schemetic:
+    """
+    symbols, parts_sum = r'[^\w\.]', 0
+
+    for sym in re.finditer(symbols, engine_line):
         span = sym.span()
         prev_symbols.append(span)  # track found symbols
 
         if prev_input is not None:
-            prev_slice = prev_input[span[0] - 3:span[1] + 3]
-            part_numbers += re.findall(r'\d+', prev_slice)
+            parts_sum += find_part_numbers(span, prev_input)
 
-        # find numbers before or after symbols (not dot)
-        input_slice = input_line[span[0] - 3:span[1] + 3]
-        part_numbers += re.findall(r'\d+', input_slice)
+        parts_sum += find_part_numbers(span, engine_line)
 
-    return sum([int(x) for x in part_numbers])
+    return parts_sum
 
 
 for input_line in inputs:
@@ -44,12 +54,10 @@ for input_line in inputs:
 
     while len(prev_symbols) > 0:
         prev_span, prev_symbols = prev_symbols[0], prev_symbols[1:]
-        input_slice = input_line[prev_span[0] - 3:prev_span[1] + 3]
-        new_part_numbers = re.findall(r'\d+', input_slice)
-        parts_sum += sum([int(x) for x in new_part_numbers])
+        parts_sum += find_part_numbers(prev_span, input_line)
         print(parts_sum, end=" + ")
 
-    parts_sum += find_part_numbers(input_line)
+    parts_sum += unwind_schemetic(input_line)
     print(parts_sum)
 
     answer += parts_sum
