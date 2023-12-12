29a6a3ecebdf3665ec4a6a791e61c70ff644d91a
jiotaro <monkeydjiotaro@gmail.com>
2023-12-12 13:55:30 +0700

    day.3 try to identify controlling symbols first

--- day_3.py
+++ day_3.py
@@ -4,16 +4,20 @@ This adheres a solution of Day 3 puzzle
 """
 
 from os import getcwd
+import re
+
+symbols = r'[^\w\.]'  # not alphanumeric and dot
 
 answer = 0  # hold the expected output
 
 input_file = f"{getcwd()}/input.txt"
 inputs = (i for i in open(input_file))
 
-
 for input_line in inputs:
     input_line = input_line.strip()
-    print(input_line)
+    print(input_line, end=" --> ")
+
+    print(f"sym-ctrl: {re.findall(symbols, input_line)}")
 
     # answer += game_id
 
