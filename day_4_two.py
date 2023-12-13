dabaa2094569baa66526a0b6a2dfb0bcae1353f7
jiotaro <monkeydjiotaro@gmail.com>
2023-12-14 00:04:37 +0700

    day.4#2 fix outputs

--- day_4_two.py
+++ day_4_two.py
@@ -11,23 +11,22 @@ scratchcards = open(argv[1]).read().split('\n')
 winnng_copies = [0] * len(scratchcards)
 
 for i, card in enumerate(scratchcards):
-    print(card, end=" --> ")
 
-    winning, having = re.sub(r"Card \d+: ", "", card).split(" | ")
+    card_no, winning, having = re.split(r"\s?[:|] ", card)
     matched = [n for n in having.split() if n in winning.split()]
 
-    print(matched, end=" = ")
+    print(card_no + ":", matched, end=" = ")
 
     scored = 0 if len(matched) == 0 else 2 ** (len(matched) - 1)
     if scored > 0:
         # win a copy of itself first
         winnng_copies[i] += 1
-    # then, win a copy of next card
+    # then, win that many copies of next card
     for n in range(len(matched)):
         # (Cards will never make you copy a card past the end of the table.)
-        winnng_copies[i + n + 1] += 1
+        winnng_copies[i + n + 1] += winnng_copies[i]
 
-    print(scored, "copies=", winnng_copies[i])
+    print("scored=", scored, "copies=", winnng_copies[i])
 
 answer = sum(winnng_copies)
 print(f"[Total Score Is: {answer}]")
