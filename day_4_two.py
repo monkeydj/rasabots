268f6886ef648cb9e9b4c7996d847af0a66045f0
jiotaro <monkeydjiotaro@gmail.com>
2023-12-14 00:17:56 +0700

    day.4#2 make it more elegant-ish

--- day_4_two.py
+++ day_4_two.py
@@ -8,18 +8,18 @@ from sys import argv
 import re
 
 scratchcards = open(argv[1]).read().split('\n')
-having_copies = [1] * len(scratchcards)  # even not yet won
+having_copies = [1] * len(scratchcards)  # initially own 1 of each card
 
 for i, card in enumerate(scratchcards):
 
-    card_no, winning, having = re.split(r"\s?[:|] ", card)
-    matched = [n for n in having.split() if n in winning.split()]
+    card_no, winning, having = re.split(r" ?[:|] ", card)
+    # ref: https://github.com/jonathanpaulson/AdventOfCode/blob/master/2023/4.py#L14
+    matched = set(having.split()) & set(winning.split())
 
-    print(card_no + ":", matched, end=" = ")
+    print(card_no + ":", matched, end=" ")
 
-    scored = 0 if len(matched) == 0 else 2 ** (len(matched) - 1)
-
-    # win that many copies of next card
+    scored = 0 if not matched else 2 ** (len(matched) - 1)
+    # and win that many copies of next card(s)
     for n in range(len(matched)):
         # (Cards will never make you copy a card past the end of the table.)
         having_copies[i + n + 1] += having_copies[i]
