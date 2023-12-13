d49db2d2888607b5f4dfb9f7d33361e35e03698e
jiotaro <monkeydjiotaro@gmail.com>
2023-12-13 22:47:10 +0700

    day.3#2 much more cleaner code but not yet passed

--- day_3_two.py
+++ day_3_two.py
@@ -10,47 +10,39 @@ from sys import argv
 import re
 
 
-def mark(part_numbers: list[re.Match], output_line: str = ""):
-    """
-    Print out found part_numbers highlighted in output_line.
-    """
-    chunk_start = 0
-    # as they are found not in order of appearance in output_line
-    part_numbers_ordered = sorted(part_numbers, key=lambda n: n.start())
-
-    for number in part_numbers_ordered:
-        print(output_line[chunk_start:number.start()], end="")
-        # ref: https://stackoverflow.com/a/51708889/8546076
-        print("\033[4m\033[92m" + number.group(0) + "\033[0m", end="")
-
-        chunk_start = number.end()
-
-    print(output_line[chunk_start:])  # whatever left
-
-
 def get_area_indexes(r_i: int, start: int, end: int) -> list[tuple]:
     rows, cols = [r_i + r for r in [-1, 0, 1]], range(start, end + 1)
     return [(x, y) for x in rows for y in cols]
 
 
 # --- main process ----
+
 engine_schematic = open(argv[1]).read().split()
 found_gear_parts = defaultdict(list)
+
+answer = 0
 
 for cr, engine_line in enumerate(engine_schematic):
     for number in re.finditer(r'\d+', engine_line):
         area = get_area_indexes(cr, number.start(), number.end())
+
+        print(number, area)
 
         for x, y in area:
             if any([not (0 <= x < len(engine_schematic)),
                    not (0 <= y < len(engine_schematic[cr]))]):
                 continue
 
-            if engine_schematic[x][y] == '*':
-                print(number)
+            if engine_schematic[x][y] != '*':
+                continue
 
+            found_gear_parts[(x, y)].append((cr, number))
 
-# mark(found_part_numbers, prev_line)  # very last line
+            print((x, y, found_gear_parts[(x, y)]))
 
-answer = 0  # //TODO: calculate the final answer here
+            if len(found_gear_parts[(x, y)]) == 2:
+                part_numbers = [int(number.group(0))
+                                for cr, number in found_gear_parts[(x, y)]]
+                answer += part_numbers[0] * part_numbers[1]
+
 print(f"[ Total Gear Ratio Is: {answer} ]")
