a6ea164359e72ae5623988de607e7da45c7e17ad
jiotaro <monkeydjiotaro@gmail.com>
2023-12-14 00:20:10 +0700

    day.5 prepare for battle with a soil map

---
+++ day_5.py
@@ -0,0 +1,16 @@
+"""
+This adheres a solution of Day 5 puzzle
+- https://adventofcode.com/2023/day/5
+"""
+
+from sys import argv
+
+inputs = open(argv[1]).read().split('\n')
+
+answer = 0
+
+for line in inputs:
+    print(line)
+
+
+print(f"[Total Score Is: {answer}]")
