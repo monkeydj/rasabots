dac2bb5bd3033aa3d3a1a87d258256577be08c8f
jiotaro <monkeydjiotaro@gmail.com>
2023-12-14 01:25:37 +0700

    day.5 know get_src_pos but find it hard to read inputs

--- day_5.py
+++ day_5.py
@@ -5,6 +5,14 @@ This adheres a solution of Day 5 puzzle
 
 from sys import argv
 
+# ideally, follow this stream to find seed's location:
+# seed -> soil -> fertilizer -> water -> light -> temperature -> humidity -> location
+
+
+def get_src_pos(dest_pos: int, dest: int, src: int, length: int) -> int:
+    return src + (dest_pos - dest) if dest <= dest_pos < dest + length else -1
+
+
 inputs = open(argv[1]).read().split('\n')
 
 answer = 0
