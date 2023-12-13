9e83e8826b28b72b05efdca6e71d45148b4bd4f1
jiotaro <monkeydjiotaro@gmail.com>
2023-12-13 23:23:50 +0700

    day.4 pass test

--- day_4.py
+++ day_4.py
@@ -5,7 +5,26 @@ This adheres a solution of Day 4 puzzle
 
 from sys import argv
 
+import re
+
 scratchcards = open(argv[1]).read().split('\n')
 
+answer = 0
+
 for card in scratchcards:
-    print(card)
+    print(card, end=" --> ")
+
+    winning, having = re.sub(r"Card \d+: ", "", card).split(" | ")
+    winning, having = winning.split(), having.split()
+    print(winning, "x", having, end=" = ")
+
+    scored = 0
+    for n in having:
+        if n in winning:
+            scored = 1 if scored == 0 else scored * 2
+
+    print(scored)
+
+    answer += scored
+
+print(f"[Total Score Is: {answer}]")
