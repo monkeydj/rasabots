8ccacdd4e3c612ca26ee582b2e9432f323c3104d
jiotaro <monkeydjiotaro@gmail.com>
2023-12-14 23:50:48 +0700

    day.5#2 almost final solution

--- day_5_two.py
+++ day_5_two.py
@@ -6,14 +6,15 @@ This adheres a solution of Day 5 puzzle Part Two
 from sys import argv
 
 
-def find_dest_pos(src_pos: int, dest_start: int, src_start: int, length: int) -> int:
-    if src_start <= src_pos < src_start + length:
-        return dest_start + (src_pos - src_start)
+def find_dest_pos(src_pos: int, src_dest_ranges: list[set]) -> int:
+    for dest_start, src_start, length in src_dest_ranges:
+        if src_start <= src_pos < src_start + length:
+            return dest_start + (src_pos - src_start)
 
     return src_pos
 
 
-seeds, *maps = open(argv[1]).read().split('\n\n')
+seeds, *maps = open("test.in").read().split('\n\n')
 # (it just changes the format of seed range, but the data amount is huge!)
 
 # ideally, it just follow downstream to find seed's location:
@@ -26,17 +27,19 @@ for categories_map in maps:
 
     for i in range(0, len(ranges), 3):  # always multiple of 3
         dest_start, src_start, length = map(int, ranges[i:(i + 3)])
-        downstream.append((src_to_dest, (dest_start, src_start, length)))
+        downstream.append((dest_start, src_start, length))
 
         print(src_to_dest, "/ map=", (dest_start, src_start, length))
 
-seed_ranges = list(map(int, seeds.split()[1:]))
+
+seed_ranges, min_pos = seeds.split()[1:], float("inf")
 
 for i in range(0, len(seed_ranges), 2):
-    seed_start, seed_range = seed_ranges[i], seed_ranges[i + 1]
+    seed_start, seed_length = int(seed_ranges[i]), int(seed_ranges[i + 1])
 
-    print("seeds=", f"({seed_start} + {seed_range})")
+    for seed in range(seed_start, seed_start + seed_length):
+        min_pos = min(min_pos, find_dest_pos(seed, downstream))
 
+    print("seeds=", f"({seed_start} + {seed_length})", "min_pos=", min_pos)
 
-answer = 0
-print(f"[Total Score Is: {answer}]")
+print(f"[Min Position Is: {min_pos}]")
