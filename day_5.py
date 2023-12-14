869e31fce197f6168334eab6abe19dbb1ecab5be
jiotaro <monkeydjiotaro@gmail.com>
2023-12-14 22:44:24 +0700

    day.5 get some missing insights

--- day_5.py
+++ day_5.py
@@ -11,7 +11,7 @@ def find_dest_pos(src_pos: int, dest: int, src: int, length: int) -> int:
 
 
 seeds, *maps = open(argv[1]).read().split('\n\n')
-positions = [int(s) for s in seeds.split()[1:]]
+positions = map(int, seeds.split()[1:])
 
 print("seeds=", positions)
 
@@ -22,7 +22,7 @@ for categories_map in maps:
     # ? if the maps are not in order of downstream
 
     for i in range(0, len(ranges), 3):
-        src, dest, length = [int(n) for n in ranges[i:(i + 3)]]
+        src, dest, length = map(int, ranges[i:(i + 3)])
         positions = [find_dest_pos(p, src, dest, length) for p in positions]
 
         print(src_to_dest + "=", positions, "/ map=", [src, dest, length])
