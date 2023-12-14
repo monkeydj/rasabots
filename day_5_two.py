ad990934ed6b1f467dc650de2fe08a8239ca5773
jiotaro <monkeydjiotaro@gmail.com>
2023-12-15 00:17:20 +0700

    day.5#2 a naive thought of efficient process, but long runtime

--- day_5_two.py
+++ day_5_two.py
@@ -6,30 +6,36 @@ This adheres a solution of Day 5 puzzle Part Two
 from sys import argv
 
 
-def find_dest_pos(src_pos: int, src_dest_ranges: list[set]) -> int:
-    for dest_start, src_start, length in src_dest_ranges:
-        if src_start <= src_pos < src_start + length:
-            return dest_start + (src_pos - src_start)
+def lookup(seed_pos: int, almanac: list[set]) -> int:
+    position, prev_done_map = seed_pos, None
 
-    return src_pos
+    for map_name, dest_start, src_start, length in almanac:
+        if map_name == prev_done_map:
+            continue
+        # else it keeps checking the next map in almanac
+        if src_start <= position < src_start + length:
+            position = dest_start + (position - src_start)
+            prev_done_map = map_name
+
+    return position
 
 
-seeds, *maps = open("test.in").read().split('\n\n')
+seeds, *maps = open(argv[1]).read().split('\n\n')
 # (it just changes the format of seed range, but the data amount is huge!)
 
 # ideally, it just follow downstream to find seed's location:
 # seed -> soil -> fertilizer -> water -> light -> temperature -> humidity -> location
-downstream = []
+almanac = []
 
 # ? if the maps are not in order of downstream
 for categories_map in maps:
-    src_to_dest, _, *ranges = categories_map.split()
+    map_name, _, *ranges = categories_map.split()
 
     for i in range(0, len(ranges), 3):  # always multiple of 3
         dest_start, src_start, length = map(int, ranges[i:(i + 3)])
-        downstream.append((dest_start, src_start, length))
+        almanac.append((map_name, dest_start, src_start, length))
 
-        print(src_to_dest, "/ map=", (dest_start, src_start, length))
+        print(map_name, "/ map=", (dest_start, src_start, length))
 
 
 seed_ranges, min_pos = seeds.split()[1:], float("inf")
@@ -38,7 +44,7 @@ for i in range(0, len(seed_ranges), 2):
     seed_start, seed_length = int(seed_ranges[i]), int(seed_ranges[i + 1])
 
     for seed in range(seed_start, seed_start + seed_length):
-        min_pos = min(min_pos, find_dest_pos(seed, downstream))
+        min_pos = min(min_pos, lookup(seed, almanac))
 
     print("seeds=", f"({seed_start} + {seed_length})", "min_pos=", min_pos)
 
