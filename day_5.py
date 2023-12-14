638673535cc2fbc0b6ce5956e6b2e0f2efd13cee
jiotaro <monkeydjiotaro@gmail.com>
2023-12-14 14:30:56 +0700

    day.5 parse the input rightly now

--- day_5.py
+++ day_5.py
@@ -5,6 +5,7 @@ This adheres a solution of Day 5 puzzle
 
 from sys import argv
 
+
 # ideally, follow this stream to find seed's location:
 # seed -> soil -> fertilizer -> water -> light -> temperature -> humidity -> location
 
@@ -13,12 +14,15 @@ def get_src_pos(dest_pos: int, dest: int, src: int, length: int) -> int:
     return src + (dest_pos - dest) if dest <= dest_pos < dest + length else -1
 
 
-inputs = open(argv[1]).read().split('\n')
+seeds, *maps = open(argv[1]).read().split('\n\n')
+seeds = [int(s) for s in seeds.split()[1:]]
+
+print("seeds=", seeds)
+
+for line in maps:
+    src_to_dest, _, *ranges = line.split()
+    # src, dest = re.search(r'^(\w+)-to-(\w+)', line)
+    print(src_to_dest + "=", ranges)
 
 answer = 0
-
-for line in inputs:
-    print(line)
-
-
 print(f"[Total Score Is: {answer}]")
