bd305d7c3659f9f39bd5828203a3c482f8a983a6
jiotaro <monkeydjiotaro@gmail.com>
2023-12-17 22:34:41 +0700

    day.6#2 it solved in an accepteble way

---
+++ day_6p2.py
@@ -0,0 +1,30 @@
+"""
+This adheres a solution of Day 6 puzzle Part Two
+- https://adventofcode.com/2023/day/6#part2
+"""
+
+
+def outrun(time: int, distance: int) -> int:
+    beats, speed = 0, 0
+    # speed increases by one millimeter per millisecond
+    # speed = 0 or eq time cannot make a run, thus don't beat it
+    while speed < time:
+        speed += 1
+        if speed * (time - speed) > distance:
+            beats += 1
+
+    return beats
+
+
+def rekern(input_line: str) -> str:
+    return int(input_line.replace(" ", "").split(":")[1])
+
+
+times, distances = open("data.in").read().split("\n")
+time, distance = (rekern(times), rekern(distances))
+
+print(f"record: {time} ms / {distance} mm")
+
+answer = outrun(int(time), int(distance))
+
+print(f"[Total Score Is: {answer}]")
