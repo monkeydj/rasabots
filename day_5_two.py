43216f321b2b0c8160d36f06a222f763effce754
jiotaro <monkeydjiotaro@gmail.com>
2023-12-17 21:28:16 +0700

    day.5#2 try to make lookup easier

--- day_5_two.py
+++ day_5_two.py
@@ -10,30 +10,29 @@ Some references for efficient range lookup:
 from sys import argv
 
 
-def lookup(s_start: int, s_length: int, almanac: list[set]) -> list[set]:
-    prev_done_map, locations = None, [[(s_start, s_start + s_length - 1)], []]
+def lookup(s_start: int, s_length: int, almanac: list[list[set]]) -> list[set]:
+    locations = [[(s_start, s_start + s_length - 1)]]
 
-    for map_name, dest_start, src_start, length in almanac:
-        if map_name == prev_done_map:
-            locations.append([])
-            continue
+    for dest_loc in almanac:
+        locations.append([])  # in new map
 
-        src_end = src_start + length - 1
+        for dest_start, src_start, length in dest_loc:
+            src_end = src_start + length - 1
 
-        for start, end in locations[-2]:  # loop through src locations
+            for start, end in locations[-2]:  # loop through src locations
 
-            if start > src_end or end < src_start:
-                continue  # not in map
+                if start > src_end or end < src_start:
+                    continue  # not in map
 
-            junction_length = min(end, src_end) - max(s_start, src_start)
+                junction_length = min(end, src_end) - max(s_start, src_start)
 
-            mapped_start = dest_start + (s_start - src_start)
-            mapped_end = mapped_start + junction_length - 1
+                mapped_start = dest_start + (s_start - src_start)
+                mapped_end = mapped_start + junction_length - 1
 
-            locations[-1].append((mapped_start, mapped_end))
-        else:  # lookup is ended,
-            # start checking the next map in almanac
-            prev_done_map = map_name
+                locations[-1].append((mapped_start, mapped_end))
+            else:  # lookup is ended,
+                # start checking the next map in almanac
+                pass
 
     return locations[-1]  # very last collection is desired locations
 
@@ -49,9 +48,11 @@ almanac = []
 for categories_map in maps:
     map_name, _, *ranges = categories_map.split()
 
+    almanac.append([])  # try to make lookup easier
+
     for i in range(0, len(ranges), 3):  # always multiple of 3
         dest_start, src_start, length = map(int, ranges[i:(i + 3)])
-        almanac.append((map_name, dest_start, src_start, length))
+        almanac[-1].append((dest_start, src_start, length))
 
         print(map_name, "/ map=", (dest_start, src_start, length))
 
