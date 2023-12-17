dc4b3eb2c2b2a1dcd81b0e550898bbee6a3d5b0c
jiotaro <monkeydjiotaro@gmail.com>
2023-12-17 22:24:27 +0700

    day.6 outran the test data

--- day_6.py
+++ day_6.py
@@ -4,15 +4,27 @@ This adheres a solution of Day 6 puzzle
 """
 
 
-def charge(time: int, distance: int):
-    pass
+def outrun(time: int, distance: int) -> int:
+    beats, speed = 0, 0
+    # speed increases by one millimeter per millisecond
+    # speed = 0 or eq time cannot make a run, thus don't beat it
+    while speed < time:
+        speed += 1
+        if speed * (time - speed) > distance:
+            beats += 1
+
+    return beats
 
 
 times, distances = open("data.in").read().split("\n")
 records = list(zip(times.split()[1:], distances.split()[1:]))
 
+answer = 0
 for time, distance in records:
     print(f"record: {time} ms / {distance} mm")
 
-answer = 0
+    beat_it = outrun(int(time), int(distance))
+    if beat_it:
+        answer = (answer or 1) * beat_it
+
 print(f"[Total Score Is: {answer}]")
