74e32b63f32db1f45fe71ae9b4cc4c46f4a314d7
jiotaro <monkeydjiotaro@gmail.com>
2023-12-18 23:19:52 +0700

    day.8p2 fix according to least common multiple approach (not sure why)

--- day_8p2.py
+++ day_8p2.py
@@ -3,6 +3,8 @@ This adheres a solution of Day 8 puzzle PART Two
 - https://adventofcode.com/2023/day/8#part2
 """
 
+import math
+
 
 def parse(n: str) -> set:
     return (lambda a, b: (a, b[1:-1].split(", ")))(*n.split(" = "))
@@ -14,20 +16,22 @@ def traverse(instruct: str):
             yield 0 if i == "L" else 1
 
 
+def find_z(n: str, nodes: dict, instruct: str) -> int:
+    step, z, cnt = traverse(instruct), n, 0
+
+    while z[-1] != "Z":
+        z, cnt = nodes[z][next(step)], cnt + 1
+
+    return cnt
+
+
 instruct, _, *network = open("data.in").read().split("\n")
 nodes = dict([parse(n) for n in network])
+
 travel_n = [n for n in nodes.keys() if n[-1] == "A"]
-steps, step = 0, traverse(instruct)
-
-while True:
-    lr, steps = next(step), steps + 1
-    print(f"{travel_n}", end=f" =[{lr}]=> ")
-
-    travel_n = [nodes[n][lr] for n in travel_n]
-    print(travel_n)
-
-    if sum(1 for n in travel_n if n[-1] == "Z") == len(travel_n):
-        break
-
+cnt_z = [find_z(n, nodes, instruct) for n in travel_n]
+# interestingly, individuals can be solved separately
+# https://github.com/womogenes/AoC-2023-Solutions/blob/main/day_08/day_08_p2.py
+steps = math.lcm(*cnt_z)
 
 print(f"[Total Steps Is: {steps}]")
