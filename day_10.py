cd0e8fa2502a367d4fb25a274d49657e1cb33f86
jiotaro <monkeydjiotaro@gmail.com>
2023-12-19 11:05:34 +0700

    day.10 read data.in

---
+++ day_10.py
@@ -0,0 +1,11 @@
+"""
+This adheres a solution of Day 10 puzzle
+- https://adventofcode.com/2023/day/10
+"""
+
+pipe_maze = open("data.in").read().split()
+
+for p in pipe_maze:
+    print(p)
+
+print(f"[Extrapolated Value Is: {0}]")
