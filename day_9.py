a09afa6c78872a30b48d2dab6ac07956e020a123
jiotaro <monkeydjiotaro@gmail.com>
2023-12-19 10:18:38 +0700

    day.9 thought it should be quickly solved

---
+++ day_9.py
@@ -0,0 +1,25 @@
+"""
+This adheres a solution of Day 9 puzzle
+- https://adventofcode.com/2023/day/9
+"""
+
+
+def diff(values: list[int]) -> list[int]:
+    return [values[i+1] - v for i, v in enumerate(values[:-1])]
+
+
+def traverse(history: str):
+    stacks = [list(map(int, history.split()))]
+
+    while True:
+        stacks.append(diff(stacks[-1]))
+        if all(s == 0 for s in stacks[-1]):
+            break
+
+    return sum(s[-1] for s in stacks[:-1:-1])
+
+
+histories = open("data.in").read().split("\n")
+preds_n = [traverse(h) for h in histories]
+
+print(f"[Total Steps Is: {sum(preds_n)}]")
