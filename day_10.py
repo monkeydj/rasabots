114b38df54b4007535eda3f3727f50a42205fd15
jiotaro <monkeydjiotaro@gmail.com>
2023-12-26 21:50:57 +0700

    day.10 somehow it doesn't get me started thinking

--- day_10.py
+++ day_10.py
@@ -3,8 +3,34 @@ This adheres a solution of Day 10 puzzle
 - https://adventofcode.com/2023/day/10
 """
 
-pipe_maze = open("data.in").read().split()
 
+def find_adjacent_pipes(pipes: list[str], row: int, col: int) -> list[list[int]]:
+    adjacents = []
+
+    match pipes[row][col]:
+        case '|':
+            adjacents.append([row + 1, col], [row - 1, col])
+        case '-':
+            adjacents.append([row, col + 1], [row, col - 1])
+        case 'L':
+            adjacents.append([row, col + 1], [row - 1, col])
+        case 'J':
+            adjacents.append([row, col - 1], [row - 1, col])
+        case '7':
+            adjacents.append([row, col - 1], [row + 1, col])
+        case 'F':
+            adjacents.append([row, col + 1], [row + 1, col])
+        case _:  # it must be ground
+            pass
+
+    return adjacents
+
+
+def trace_pipes(s_x: int, s_y: int, pipe_maze: list[str]) -> int:
+    pass
+
+
+pipe_maze = open("data.in").read().split()
 s_point = None
 
 for i, p in enumerate(pipe_maze):
