0d4c1ff00f4b6a7014846232752bd1b2479c9f1a
jiotaro <monkeydjiotaro@gmail.com>
2024-05-20 23:24:35 +0700

    feat: structurize into CliArgs

--- my-grrs/src/main.rs
+++ my-grrs/src/main.rs
@@ -1,6 +1,16 @@
+struct CliArgs {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern");
     let path = std::env::args().nth(2).expect("no file to search");
 
-    println!("pattern: {:?}, path: {:?}", pattern, path);
+    let args = CliArgs {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
