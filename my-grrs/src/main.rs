7d1f744f84b19f4fe9c0a029932b8b2aa3169f35
jiotaro <monkeydjiotaro@gmail.com>
2024-05-21 23:36:40 +0700

    feat: add help texts and explicit option names

--- my-grrs/src/main.rs
+++ my-grrs/src/main.rs
@@ -1,8 +1,13 @@
 use clap::Parser;
 
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct CliArgs {
+    /// The pattern to look for in designated file
+    #[arg(short = 'x', long)]
     pattern: String,
+    /// The path to file to be searched
+    #[arg(short = 'p', long)]
     path: std::path::PathBuf,
 }
 
