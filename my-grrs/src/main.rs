52a5cfb1bb40437b31d6605b0e7e3dcec983f710
jiotaro <monkeydjiotaro@gmail.com>
2024-05-21 23:32:46 +0700

    feat: use clap for parsing args

--- my-grrs/src/main.rs
+++ my-grrs/src/main.rs
@@ -1,16 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct CliArgs {
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern");
-    let path = std::env::args().nth(2).expect("no file to search");
-
-    let args = CliArgs {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = CliArgs::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
