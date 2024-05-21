877b9860e161308899a455dc9bc26eeb02f6660c
jiotaro <monkeydjiotaro@gmail.com>
2024-05-22 00:13:18 +0700

    refactor: use shortcut of returning error

--- my-grrs/src/main.rs
+++ my-grrs/src/main.rs
@@ -15,13 +15,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CliArgs::parse();
     // println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 
-    let result = std::fs::read_to_string(args.path);
-    let content = match result {
-        Ok(content) => content,
-        Err(error) => return Err(error.into()),
-    };
+    // let result = std::fs::read_to_string(args.path);
+    // let content = match result {
+    //     Ok(content) => content,
+    //     Err(error) => return Err(error.into()),
+    // };
     // or shortcut for above 2 expressions
     // let content = std::fs::read_to_string(args.path).unwrap();
+    // same as
+    let content = std::fs::read_to_string(args.path)?;
 
     println!("The file content:\n{}", content);
 
