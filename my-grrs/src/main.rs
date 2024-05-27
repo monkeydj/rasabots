0b185468ecf15c6a31721a62aa0566d22cce3b37
jiotaro <monkeydjiotaro@gmail.com>
2024-05-27 11:59:11 +0700

    refactor: app logic into lib.rs

--- my-grrs/src/main.rs
+++ my-grrs/src/main.rs
@@ -12,21 +12,6 @@ struct CliArgs {
     path: std::path::PathBuf,
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line).unwrap();
-        }
-    }
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CliArgs::parse();
     // println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
@@ -47,7 +32,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("The file content:\n{}", content);
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    my_grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
 }
