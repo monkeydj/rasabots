92bb5071d8e39366a6907ad1fc998e6f666c637a
jiotaroth <monkeydjiotaro@gmail.com>
2024-10-14 18:45:53 +0700

    chore: optimize cells & small updates to models

--- notebooks/modeles.py
+++ notebooks/modeles.py
@@ -1,10 +1,12 @@
-# Any data models to be used in main notebook.
-# Also some small utilities.
+# Any data models to be used in main notebooks.
+# With utilities.
 
 MOVEMENT_HIST = [
     'ts',
-    'color',
+    'ball_color',
+    # this coordination will be relative to table
     'x', 'y', 'z',  # point at time
+    # these vectors are centered at ball centre
     'dir_x', 'dir_y', 'dir_z',  # vector direction end
     'v_x', 'v_y', 'v_z',  # velocity vector
     'sp_x', 'sp_y', 'sp_z'  # spinning vector
@@ -13,6 +15,7 @@ MOVEMENT_HIST = [
 CUE_SPECS = [
     'material',
     'tip_size',
+    'tip_hardness',
     'weight',
     'balance_point'
 ]
